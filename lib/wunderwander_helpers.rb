2f84ce8ea0d00e62aa0afbcf9c055480e6f377ee
Daniël <wander-wonder@foldingbeauty.nl>
2019-03-05 19:09:34 +0100

    refactor and use helm

--- lib/wunderwander_helpers.rb
+++ lib/wunderwander_helpers.rb
@@ -4,6 +4,6 @@
 module WunderWanderHelpers
   VERSION = '0.1.4-dev'
   BASE_IMAGE = 'foldingbeauty/wunderwander-gitops'
-  IMAGE = "#{BASE_IMAGE}:#{VERSION}"
+  IMAGE = "#{BASE_IMAGE}:#{VERSION}" || ENV['GITOPS_IMAGE']
   DEFAULT_PULL_FREQENCY = 10
 end
